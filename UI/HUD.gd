8accc94380f3dcebfc9377fdb6c5a700806c3de1
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-08-02 22:34:01 -0300

    [fix]: hud health bar damage interaction

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -2,24 +2,18 @@ class_name HUD
 extends CanvasLayer
 
 @onready var timer_label: Label = %TimerLabel
-@onready var hp_progress_bar: TextureProgressBar = $hpBar
+@onready var hp_progress_bar: TextureProgressBar = $Panel/hpBar
 
 var time_elapsed: float = 0.0
 
-
 func _ready():
-	#GameManager.player.max_hp.connect(hp_update)
-	#GameManager.player.hp.connect(hp_update)
-	pass
+	GameManager.player.hp_changed.connect(hp_update)
+
 
 func _process(delta):
 	timer_label.text = GameManager.time_elapsed_string
-	
-	# update hp (to-do)
-	#hp_progress_bar.max_value = max_hp
-	#hp_progress_bar.value = hp
 
 ## hp update
-#func hp_update(hp: int, max_hp: int):
-	#hp_progress_bar.max_value = max_hp
-	#hp_progress_bar.value = hp
+func hp_update(hp: int, max_hp: int):
+	hp_progress_bar.max_value = max_hp
+	hp_progress_bar.value = hp
